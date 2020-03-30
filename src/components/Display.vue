af76a0594d738bf627456e15c7829cd6ab072e97
alanfxx <j.alands2@gmail.com>
2020-03-30 08:39:17 -0300

    Layout criado

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="display">
-      0
+      {{value}}
   </div>
 </template>
 
 <script>
 export default {
-
+    props: ['value']
 }
 </script>
 
 <style>
-
+.display {
+    grid-column: span 4;
+    font-size: 2.1em;
+    background-color: #0004;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 20px;
+    overflow: hidden;
+}
 </style>
